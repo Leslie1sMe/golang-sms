5220f2ab378192da3c181bbdcd02bae40734cef3
lizhonglin <lizhonglin_melody@yeah.net>
2019-03-15 17:42:40 +0800

    golang package for luosimao sms

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+func main() {
+	//var user = sms.SendParam{ApiKey: "key-XXXXXXXXXXXXXXXXXXXXXXXX", UseSsl: true}
+	//user.Send("18519268986", "验证码：19272【铁壳测试】")
+	//var mobileList = []string{"18519268986", "18519268986"}
+	//user.SendBatch(mobileList, "asdasdasdasdasdasd【PUPUPULA】")
+	//user.CheckDeposit()
+}
